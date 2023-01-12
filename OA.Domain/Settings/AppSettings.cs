6d74d23a2af00d3c7f1127a45a913e77fcae0e61
ahmad karami <karami1400@gmail.com>
2023-01-12 22:24:11 +0330

    first commit

---
+++ OA.Domain/Settings/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Domain.Settings
+{
+    public class AppSettings
+    {
+        public ApplicationDetail ApplicationDetail { get; set; }
+        public Serilog Serilog { get; set; }
+        public string AllowedHosts { get; set; }
+        public ConnectionStrings ConnectionStrings { get; set; }
+        public FeatureManagement FeatureManagement { get; set; }
+        public MailSettings MailSettings { get; set; }
+        public JWTSettings JWTSettings { get; set; }
+    }
+}
---
+++ OA.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Domain.Configurations;
+using OA.Domain.Entities;
+using OA.Domain.Settings;
+using System.Threading.Tasks;
+
+namespace OA.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        AppSettings _appSettings= new AppSettings();
+
+        public ApplicationDbContext()
+        {
+
+        }
+        
+     
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, AppSettings appSettings) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _appSettings = appSettings;
+        }
+
+        #region dbSets
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        #endregion
+
+        #region configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
+        }
+        #endregion
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder
+        //        .UseSqlServer(_appSettings.ConnectionStrings.OArchConnection);
+        //    }
+
+        //}
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
